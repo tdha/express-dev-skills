18e3d2c2762bd11d5e435c653b20b1efd10b2bea
Ton Ha <ton@tdha.co>
2024-02-21 01:00:14 +1000

    Skills list now a list of links

--- models/skill.js
+++ models/skill.js
@@ -3,12 +3,12 @@ const skills = [
     {id: 101, skill: 'CSS', acquired: true},
     {id: 102, skill: 'Javascript', acquired: true},
     {id: 103, skill: 'GitHub', acquired: true},
-    {id: 104, skill: 'NodeJS', acquired: true},
-    {id: 105, skill: 'Express', acquired: true},
-    {id: 106, skill: 'MongoDB', acquired: false},
-    {id: 200, skill: 'React', acquired: false},
-    {id: 201, skill: 'AJAX', acquired: false},
-    {id: 202, skill: 'API', acquired: false}
+    {id: 202, skill: 'NodeJS', acquired: true},
+    {id: 204, skill: 'Express', acquired: true},
+    {id: 205, skill: 'MongoDB', acquired: false},
+    {id: 302, skill: 'React', acquired: false},
+    {id: 304, skill: 'AJAX', acquired: false},
+    {id: 306, skill: 'API', acquired: false}
   ];
 	
   module.exports = {
@@ -17,4 +17,4 @@ const skills = [
 	
   function getAll() {
     return skills;
-  }
+  };
